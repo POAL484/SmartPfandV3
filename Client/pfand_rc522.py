ef1aaf5a6b5795e59c4e13b93fab53d9fa68b5b3
POAL <pbraginets48@gmail.com>
2024-02-15 19:49:49 +0300

    Second update from 15.02.24

--- Client/pfand_rc522.py
+++ Client/pfand_rc522.py
@@ -4,31 +4,37 @@ import threading as thrd
 class RFID:
     def __init__(self, cfg: dict, logger):
         self.logger = logger
-        self.device = rfid()
+        self.device = rfid(pin_irq=None)
         thrd.Thread(target=self).start()
         self.logger("RFID main thread started")
         self.uuid = [False, ""]
+        self._flagIsPresented = 0
 
     def __call__(self):
         try:
             while 1:
                 (error, data) = self.device.request()
                 if not error:
+                    if self._flagIsPresented: self._flagIsPresented = 5
                     (error, uuid) = self.device.anticoll()
-                    self.uuid = [True, uuid]
-                    self.logger(f"new card presented: {uuid}")
+                    if not self._flagIsPresented:
+                        self.uuid = [True, uuid]
+                        self.logger(f"new card presented: {uuid}")
+                else:
+                    if self._flagIsPresented: self._flagIsPresented -= 1
         except Exception as e:
             self.logger(f"ERROR --- in RFID main thread: {e}")
 
     def presentedCard(self):
-        val = tuple(self.uuid)
-        if self.uuid[0]: self.uuid[0] = False
+        val = self.uuid.copy()
+        self.uuid[0] = False
+        print(self.uuid)
         return val
 
 if __name__ == "__main__":
     from pfand_types import Logger
     from time import sleep
-    rc522 = RFID({}, Logger)
+    rc522 = RFID({}, Logger())
     while 1:
         sleep(1.5)
         print(rc522.presentedCard())
